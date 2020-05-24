f715a3b05ecd932176ee3f2477c7ffc40e474710
José Manuel Roldán <jmroldanv@gmail.com>
2020-05-24 13:19:03 +0200

    adding Sub function

--- calc/calc.go
+++ calc/calc.go
@@ -1,5 +1,11 @@
 package calc
 
+// Add sum two int parameters and return the result
 func Add(a, b int) int {
 	return a + b
+}
+
+// Sub Subtract two int parameters and return the result
+func Sub(a, b int) int {
+	return a - b
 }
